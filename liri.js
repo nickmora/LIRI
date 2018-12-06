12b2888450e2a001b5d708fc19361431d3f2459f
Nemo <bick.norales@gmail.com>
2018-12-06 10:04:42 -0800

    <spotify-this> function working, needs other functionality

--- liri.js
+++ liri.js
@@ -1,5 +1,67 @@
+//just following the README
 require("dotenv").config();
 
-var key = require("key");
-
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+//import needed packages
+// var axios = require("axios");
+
+
+//retrieves user input, stores as command and input arguments
+var command = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+
+
+//possible command inputs lead into a switch statement
+switch(command) {
+    case 'spotify-this-song':
+    {spotSearch()};
+    break;
+
+    case 'concert-this':
+    {spotSearch()};
+    break;
+
+    case 'movie-this':
+    {spotSearch()};
+    break;
+
+    case 'do-what-it-says':
+    {spotSearch()};
+    break;
+
+}
+
+
+
+
+function spotSearch() {
+    spotify.search({ type: 'track', query: input},
+    function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        var song = data.tracks.items[0];
+        getArtist(song);
+        var songName = song.name;
+        var songPreview = song.preview_url;
+        var albumName = song.album.name;
+        console.log(songName);
+        console.log(albumName);
+        console.log(songPreview);
+        
+    });
+};
+
+function getArtist(obj){
+    var artists = [];
+    obj.artists.forEach(function(artist) {
+        artists.push(artist.name);    
+    });
+    console.log(artists.join(", "));
+};
+
+
